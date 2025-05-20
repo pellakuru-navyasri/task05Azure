22438bca27056a5db02c326ce9db73d2722ef35f
Navyasri <pellakuru_navyasri@epam.com>
2025-05-20 21:58:37 +0530

    removed commands

--- task05/modules/app_service/variables.tf
+++ task05/modules/app_service/variables.tf
@@ -1,39 +1,3 @@
-/*variable "name" {
-  type = string
-}
-
-variable "location" {
-  type = string
-}
-
-variable "resource_group_name" {
-  type = string
-}
-
-variable "service_plan_id" {
-  type = string
-}
-
-variable "allowed_ip_address" {
-  description = "List of allowed IP addresses with metadata"
-  type = map(object({
-    name       = string
-    ip_address = string
-    priority   = number
-  }))
-}
-
-
-variable "tm_rule" {
-  type = object({
-    name     = string
-    priority = number
-  })
-}
-
-variable "tags" {
-  type = map(string)
-}*/
 variable "name" {
   description = "Name of the Web App"
   type        = string
--- task05/modules/traffic_manager/variables.tf
+++ task05/modules/traffic_manager/variables.tf
@@ -1,26 +1,3 @@
-/*variable "name" {
-  type = string
-}
-
-variable "resource_group_name" {
-  type = string
-}
-
-variable "routing_method" {
-  type = string
-}
-
-variable "tags" {
-  type = map(string)
-}
-
-variable "endpoints" {
-  type = map(object({
-    target_resource_id = string
-    location           = string
-    priority           = number
-  }))
-}*/
 variable "name" {
   type        = string
   description = "TM profile name"
