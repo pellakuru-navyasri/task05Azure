c4cd733b502c01b3f74d3f5b81b223158baaf030
Navyasri <pellakuru_navyasri@epam.com>
2025-05-20 15:52:38 +0530

    addes description to rg

--- task05/modules/resource_group/variables.tf
+++ task05/modules/resource_group/variables.tf
@@ -1,10 +1,13 @@
 variable "name" {
+  description = "name of resource grp"
   type = string
 }
 variable "location" {
+  description = "region"
   type = string
 }
 
 variable "tags" {
+  description = "tags to apply"
   type = map(string)
 }
